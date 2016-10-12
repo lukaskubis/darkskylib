d1c6ddf36f57fada0096f3e167d90dc4d1c56044
Lukas Kubis <contact@lukaskubis.com>
2016-10-12 14:40:52 +0200

    forecast() params check updated

--- darksky/__init__.py
+++ darksky/__init__.py
@@ -4,21 +4,35 @@ from builtins import str
 from .forecast import Forecast
 
 
-def forecast(params={}, **kwparams):
-    if not isinstance(params, dict):
-        raise TypeError("'forecast': params not in dictionary.")
+def forecast(*params, **kwparams):
+    if len(params) == 1:
+        # required params as a dictionary
+        if not isinstance(params[0], dict):
+            msg = "'params' not a dictionary ({})".format(type(params))
+            raise AttributeError(msg)
+        params = params[0]
+    elif len(params) == 3:
+        # required params as positional arguments
+        params = dict(
+            key = params[0],
+            latitude = params[1],
+            longitude = longitude[2]
+        )
+    else:
+        args = len(params)
+        msg = "forecast() doesn't take {} positional arguments".format(args)
+        raise AttributeError(msg)
 
     # merge default and explicit params
     parameters = dict(params, **kwparams)
 
-    # set mandatory parameters here
-    key = parameters.pop('key', None)
-    latitude = parameters.pop('latitude', None)
-    longitude = parameters.pop('longitude', None)
-
-    # Required parameters are missing
-    for param in ('key', 'latitude', 'longitude'):
-        if not eval(param):
-            raise ValueError('Missing parameter: ' + param)
+    # split required and optional params
+    try:
+        key = parameters.pop('key')
+        latitude = parameters.pop('latitude')
+        longitude = parameters.pop('longitude')
+    except KeyError as err:
+        msg = 'Missing parameter: {}'.format(err)
+        raise AttributeError(msg)
 
     return Forecast(key, latitude, longitude, **parameters)

55835a1ac128c455a2c061d9203444278883bb97
Lukas Kubis <contact@lukaskubis.com>
2018-01-14 01:26:59 +0100

    fix attr issue

--- darksky/__init__.py
+++ darksky/__init__.py
@@ -3,5 +3,5 @@
 from .forecast import Forecast
 
 
-def forecast(key, latitude, longitude, time=None, timeout=None **queries):
+def forecast(key, latitude, longitude, time=None, timeout=None, **queries):
     return Forecast(key, latitude, longitude, time, timeout, **queries)

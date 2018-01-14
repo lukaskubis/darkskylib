1a9e5a7b8fa7d160317b21b6fb76c4ebfe7d8486
Lukas Kubis <contact@lukaskubis.com>
2018-01-14 01:35:50 +0100

    version up

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.rst')
     README = f.read()
 
 setup(name='darkskylib',
-      version='0.3.9',
+      version='0.3.91',
       description='The Dark Sky API wrapper',
       long_description=README,
       url='https://github.com/lukaskubis/darkskylib',
